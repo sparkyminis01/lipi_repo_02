d78bc77a2c619b4086cd64edf5c83c64aceaba98
Dhanesh <dhanesh.aradhye@gmail.com>
2025-10-21 23:37:10 +0530

    Update profile-auth.js

--- profile-auth.js
+++ profile-auth.js
@@ -323,7 +323,7 @@ const ProfileManager = {
                     `<img src="${this.currentUser.photoURL}" class="user-avatar" alt="Profile">` :
                     `<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M12 12c2.7 0 4.8-2.1 4.8-4.8S14.7 2.4 12 2.4 7.2 4.5 7.2 7.2 9.3 12 12 12zm0 2.4c-3.2 0-9.6 1.6-9.6 4.8v2.4h19.2v-2.4c0-3.2-6.4-4.8-9.6-4.8z"/></svg>`
                 }
-                <span class="user-greeting">Hi, ${this.currentUser.displayName}</span>
+                <span class="user-greeting">Hello!!</span>
                 <span class="dropdown-arrow">▼</span>
             </button>
 
@@ -723,4 +723,5 @@ const ProfileManager = {
     getCurrentUser: function() {
         return this.currentUser;
     }
+
 };
